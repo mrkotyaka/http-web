148c38e079c82aa6eaa4714a790d975796dd2f24
mrkotyaka <mrkotyaka@gmail.com>
2025-05-21 15:56:19 +0300

    Initial commit

---
+++ src/main/java/net/mrKotyaka/Main.java
@@ -0,0 +1,8 @@
+package net.mrKotyaka;
+
+public class Main {
+  public static void main(String[] args) {
+    Server server = new Server();
+    server.start(9999);
+  }
+}
---
+++ src/main/java/net/mrKotyaka/Server.java
@@ -0,0 +1,4 @@
+package net.mrKotyaka;
+
+public class Server {
+}
